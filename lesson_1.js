8835347b3b989ae266022cd3c3ad1d8610d9bb3c
HAMIDJON <khamidjonashuraliev@gmail.com>
2023-06-08 23:52:28 +0900

    BRR: lesson_1 condition 2023.06.08

---
+++ lesson_1.js
@@ -0,0 +1,85 @@
+// 1-home work
+// 1- method
+// var num=4
+// if(num%2===0)  {
+//     console.log(num+ 'Juft son');
+// }
+// else{
+//     console.log(num+ 'toq son');
+// }
+// var num=5
+// if(num%2===1)  {
+//     console.log(num+ 'Juft son');
+// }
+// else{
+//     console.log(num+ 'toq son');
+// }
+//  2- method
+// let num = 10;
+// num % 2 === 0 && console.log("Juft son");
+// num % 2 !== 0 && console.log("Toq son");
+
+// 2-home work
+// 1- method
+// var ball=98
+// switch (true){
+//     case (ball >86 && ball <100):
+//     console.log("A");
+//     break;
+// }var ball=68
+// switch (true){
+//     case (ball >61 && ball <85):
+//     console.log("B");
+//     break;
+// }var ball=58
+// switch (true){
+//     case (ball >50 && ball <60):
+//     console.log("C");
+//     break;
+// }var ball=40
+// switch (true){
+//     case (ball >86 && ball <100):
+//     console.log("fail");
+//     break;
+//     default:
+//         console.log("Failed the test");
+// }
+
+// 2- method
+// let ball = 100;
+// ball >= 86 && ball <= 100 && console.log('A');
+// ball >= 61 && ball <= 85 && console.log('B');
+// ball >= 50 && ball <= 60 && console.log('C');
+// ball < 50 &&  console.log('Failed');
+
+// 3-home work
+// 1- method
+// let meva1 ="olma";
+// meva1==="olma" && console.log(10000);
+// meva1!=="anor" && console.log('bunday meva yuq');
+// let meva2 ="nok"
+// meva2==="nok" && console.log(12000);
+// // meva2 !== "uzum" && console.log('bunday meva yuq');
+// let meva3 ="tarvuz"
+// meva3==="tarvuz" && console.log(20000);
+// // meva3 !== "tarvuz" && console.log('bunday meva yuq');
+// let meva4 ="uzum"
+// meva4 !== "pomidor" && console.log('bunday meva yuq');
+
+// 2- method
+// let meva = 'anor';
+// meva === 'olma' && console.log(10000);
+// meva === 'nok' && console.log(12000);
+// meva === 'tarvuz' && console.log(15000);
+// meva !== 'olma' && meva !== 'nok' && meva !== 'tarvuz' && console.log('Bunday meva yuq');
+
+// 4-home work
+// let num = 25;
+//  if (num % 3 === 0 && num % 5 === 0 ){
+//     console.log('3 va 5ga bolinadi');
+//  }else if (num % 3 ===0){
+//     console.log('3 ga bolinadi');
+//  }
+//  else if (num % 5 === 0 ) {
+//     console.log('5ga bolinadi');
+//  }
