8daff7b78949c2e7c5822baef0a02b984c4d8462
HAMIDJON <khamidjonashuraliev@gmail.com>
2023-06-12 00:00:35 +0900

    BRR: Math method

---
+++ lesson_4.js
@@ -0,0 +1,22 @@
+// Methods
+// let son = '10.5';
+// console.log(parseInt(son));
+
+// ______________________________________
+// // methods abs
+
+// function abs(son){
+//     son >= 0 && console.log(son);
+//     son < 0 && console.log(son * (-1));
+// }
+
+// abs(-10);
+// abs(10);
+
+// math round
+// function round(son) {
+// son >= parseInt(son) +0.5 && console.log(parseInt(son) +1);
+// son < parseInt(son) +0.5 && console.log(parseInt(son));
+
+// }
+// round(10.3);
