1e05292248d7e017a106b3eadb4f74482af9d99e
HAMIDJON <khamidjonashuraliev@gmail.com>
2023-07-03 14:10:36 +0900

    feat: obj, sum, destructure

---
+++ lesson_10.js
@@ -0,0 +1,52 @@
+// // 10 home work
+// / 1- hw
+//  let obj = {
+//     name: 'WBA',
+//     age: 30,
+
+//     child: {
+//         name: ' Registan',
+//        age:10,
+//         child: {
+//             age: 5
+//         }
+//     }
+//  }
+//  let sum = 0 
+//  while(true){
+
+//     if(obj){
+      
+//         sum += obj.age
+//     }else {
+//         break
+//     }
+// obj = obj.child
+//  }
+//  console.log(sum);
+
+// 2 hw
+// let user = {
+//     login: 'ffff',
+//     parol: '12345'
+// }
+// const login = (login, parol) => { 
+//     if(login === user.login && parol === user.parol){
+// console.log('Hush keliibsiz');
+//     }else{
+// console.log('Parol yoki login hato');
+//     }
+// }
+// login('ffff', '12345')
+
+
+//  Destructure
+// let obj = {
+//     name: 'Hamidjon'
+// }
+// const {name} = obj;
+// console.log(obj.name);
+// console.log(name);
+
+// Array => Container(truck)
+//  Object => Mashina
