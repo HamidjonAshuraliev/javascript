d64005820a5e642eeaa6cf49935925d812b92495
HAMIDJON <khamidjonashuraliev@gmail.com>
2023-07-03 21:25:15 +0900

    feat: Array methods

---
+++ lesson_11.js
@@ -0,0 +1,24 @@
+//  Array methods
+//  join
+// let arr = ['a', 'b', 'c'];
+// console.log(arr.join(' - '));
+// concat
+// let arr = ['a', 'b', 'c'];
+// let arr2 = ['d', 'e', 'f'];
+// console.log(arr.concat(arr2, {name: 'hamidjon'}));
+
+
+// let arr = ['Hamidjon', 'valisher', 'Ahmad'];
+
+// arr.slice(0, 2)
+// console.log(arr);
+//  console.log(arr.slice(0,2));
+//  console.log(arr.splice(0,2));
+//  reverse
+// 1 method 
+// let str = ' WEBBRAIN';
+// let toArray =str.split('');
+// let toReverse = toArray.reverse();
+// let toJoin =toReverse.join('');
+// console.log(toJoin);
+//  T. > NIARBBEW chiqadi
