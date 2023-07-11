a2c0dbc9ab1c601b4d7d35ff97182e81074ad68b
BadBoy <131612969+hamidjonashuraliev@users.noreply.github.com>
2023-07-11 23:48:27 +0900

    Update lesson_13.js

---
+++ lesson_13.js
@@ -0,0 +1,42 @@
+//  Destructure
+// let obj = {
+//     name: 'Hamidjon'
+// }
+// const {name} = obj;
+// console.log(obj.name);
+// console.log(name);
+
+// Array => Container(truck)
+//  Object => Mashina
+
+// let arr1  = [100, 'hundred', null, undefined, {name:'hamidjon'}, ['array'], function () {}];
+// let arr2  = new Array (200) ;
+// console.log(arr2.length);
+
+//  Array methods
+//  join
+// let arr = ['a', 'b', 'c'];
+// console.log(arr.join(' - '));
+// concat
+// let arr = ['a', 'b', 'c'];
+// let arr2 = ['d', 'e', 'f'];
+// console.log(arr.concat(arr2, {name: 'hamidjon'}));
+
+// let arr = ['Hamidjon', 'valisher', 'Ahmad'];
+
+// arr.slice(0, 2)
+// console.log(arr);
+//  console.log(arr.slice(0,2));
+//  console.log(arr.splice(0,2));
+//  reverse
+// 1 method
+// let str = ' WEBBRAIN';
+// let toArray =str.split('');
+// let toReverse = toArray.reverse();
+// let toJoin =toReverse.join('');
+// console.log(toJoin);
+//  T. > NIARBBEW chiqadi
+
+//  2 method
+// let str = ' WEBBRAIN';
+// console.log(str.split('').reverse().join(''));
